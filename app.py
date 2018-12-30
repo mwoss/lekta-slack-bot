8a952149bf770416abd357224d46a16273bd6af8
mwoss <wos.mateusz16@gmail.com>
2018-12-30 20:01:45 +0100

    more changes in api, templates

--- app.py
+++ app.py
@@ -1,53 +1,56 @@
-import json
-from typing import Dict
+from typing import Dict, Any
+
+from flask import Flask, request, make_response, render_template
 
 from slack_bot.bot import Bot
-from flask import Flask, request, make_response
 
 bot = Bot()
 app = Flask(__name__)
 
 
-def _event_handler(event_type: str, slack_event: Dict[str, str]):
+def _event_handler(event_type: str, slack_event: Dict[str, Any]):
     """
     Function for handling received Slack event. Handling only known event types.
     :param event_type: Type of received Slack event
     :param slack_event: Dictionary with data from received Slack event
-    :return:
     """
     team_id = slack_event["team_id"]
-    # ================ Team Join Events =============== #
-    # When the user first joins a team, the type of event will be team_join
-    if event_type == "team_join":
-        user_id = slack_event["event"]["user"]["id"]
+    user_id = slack_event["event"]["user"]
+
+    if event_type == "message":
+        bot.answer(team_id, user_id)
+        return make_response("Response from Lekta API sent", 200, )
+    elif event_type == "member_joined_channel":
         bot.onboarding_message(team_id, user_id)
-        return make_response("Welcome Message Sent", 200, )
+        return make_response("Welcome message sent", 200, )
+    elif event_type == 'member_left_channel':
+        bot.leave_message(team_id, user_id)
+        return make_response(f"User with ID: {user_id} left channel", 200, )
 
-    # ============== Share Message Events ============= #
-    # If the user has shared the onboarding message, the event type will be
-    # message. We'll also need to check that this is a message that has been
-    # shared by looking into the attachments for "is_shared".
-    elif event_type == "message" and slack_event["event"].get("attachments"):
-        user_id = slack_event["event"].get("user")
-        if slack_event["event"]["attachments"][0].get("is_share"):
-            # Update the onboarding message and check off "Share this Message"
-            bot.update_share(team_id, user_id)
-            return make_response("Welcome message updates with shared message",
-                                 200, )
+    return make_response(f"Could not find handler for event: {event_type}", 404,
+                         {"X-Slack-No-Retry": 1})
 
-    # ============= Event Type Not Found! ============= #
-    # If the event_type does not have a handler
-    message = "You have not added an event handler for the %s" % event_type
-    # Return a helpful error message
-    return make_response(message, 200, {"X-Slack-No-Retry": 1})
+
+@app.route("/install", methods=["GET"])
+def pre_install():
+    return render_template("install.html",
+                           client_id=bot.oauth["client_id"],
+                           scope=bot.oauth["scope"])
+
+
+@app.route("/thanks", methods=["GET", "POST"])
+def thanks():
+    bot.auth(request.args.get('code'))
+    return render_template("thanks.html")
 
 
 @app.route("/events", methods=["GET", "POST"])
 def listen():
     """
-    Route for incoming events from Slack, request data contains json with event info
+    Route for incoming events from Slack, request contains json with event data
     """
-    slack_event = json.loads(request.data)
+    slack_event = request.json
+
     if "challenge" in slack_event:
         return make_response(slack_event["challenge"], 200,
                              {"content_type": "application/json"})
@@ -60,10 +63,9 @@ def listen():
         event_type = slack_event["event"]["type"]
         from pprint import pprint
         pprint(slack_event)
-        # Then handle the event by event_type and have your bot respond
         return _event_handler(event_type, slack_event)
 
-    return make_response("[NO EVENT IN SLACK REQUEST] These are not the droids you're looking for.", 404,
+    return make_response("Error. Event property not provided in response", 404,
                          {"X-Slack-No-Retry": 1})
 
 
--- slack_bot/bot.py
+++ slack_bot/bot.py
@@ -1,9 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Python Slack Bot class for use with the pythOnBoarding app
-"""
 import os
-from slack_bot import message
+
 from slackclient import SlackClient
 
 # To remember which teams have authorized your app and what tokens are
@@ -14,140 +10,39 @@ authed_teams = {}
 
 
 class Bot:
-    NAME = "lektabot"
-    EMOJI = ":robot_face:"
+    name = "lektabot"
+    emoji = ":robot_face:"
 
     def __init__(self):
-        # When we instantiate a new bot object, we can access the app
-        # credentials we set earlier in our local development environment.
         self.oauth = {"client_id": os.environ.get("CLIENT_ID"),
                       "client_secret": os.environ.get("CLIENT_SECRET"),
                       "scope": "bot"}
         self.verification = os.environ.get("VERIFICATION_TOKEN")
-
-        # NOTE: Python-slack requires a client connection to generate
-        # an oauth token. We can connect to the client without authenticating
-        # by passing an empty string as a token and then reinstantiating the
-        # client with a valid OAuth token once we have one.
         self.client = SlackClient("")
-        # We'll use this dictionary to store the state of each message object.
-        # In a production envrionment you'll likely want to store this more
-        # persistantly in a database.
-        self.messages = {}
 
-    def auth(self, code):
-        """
-        Authenticate with OAuth and assign correct scopes.
-        Save a dictionary of authed team information in memory on the bot
-        object.
-        Parameters
-        ----------
-        code : str
-            temporary authorization code sent by Slack to be exchanged for an
-            OAuth token
-        """
-        # After the user has authorized this app for use in their Slack team,
-        # Slack returns a temporary authorization code that we'll exchange for
-        # an OAuth token using the oauth.access endpoint
+    def auth(self, code: str):
         auth_response = self.client.api_call(
             "oauth.access",
             client_id=self.oauth["client_id"],
             client_secret=self.oauth["client_secret"],
             code=code
         )
-        # To keep track of authorized teams and their associated OAuth tokens,
-        # we will save the team ID and bot tokens to the global
-        # authed_teams object
         team_id = auth_response["team_id"]
-        authed_teams[team_id] = {"bot_token":
-                                     auth_response["bot"]["bot_access_token"]}
-        # Then we'll reconnect to the Slack Client with the correct team's
-        # bot token
+        authed_teams[team_id] = {"bot_token": auth_response["bot"]["bot_access_token"]}
         self.client = SlackClient(authed_teams[team_id]["bot_token"])
 
-    def open_dm(self, user_id):
-        """
-        Open a DM to send a welcome message when a 'team_join' event is
-        recieved from Slack.
-        Parameters
-        ----------
-        user_id : str
-            id of the Slack user associated with the 'team_join' event
-        Returns
-        ----------
-        dm_id : str
-            id of the DM channel opened by this method
-        """
-        new_dm = self.client.api_call("im.open",
-                                      user=user_id)
-        dm_id = new_dm["channel"]["id"]
-        return dm_id
+    def onboarding_message(self, team_id: str, user_id: str):
+        text = "Welcome to Slack! We're so glad you're here.\n" \
+               "Get started by completing the steps below."
+        channel = self._open_dm(user_id)
+        self.client.api_call("chat.postMessage",
+                             channel=channel,
+                             username=self.name,
+                             icon_emoji=self.emoji,
+                             text=text,
+                             )
 
-    def onboarding_message(self, team_id, user_id):
-        """
-        Create and send an onboarding welcome message to new users. Save the
-        time stamp of this message on the message object for updating in the
-        future.
-        Parameters
-        ----------
-        team_id : str
-            id of the Slack team associated with the incoming event
-        user_id : str
-            id of the Slack user associated with the incoming event
-        """
-        # We've imported a Message class from `message.py` that we can use
-        # to create message objects for each onboarding message we send to a
-        # user. We can use these objects to keep track of the progress each
-        # user on each team has made getting through our onboarding tutorial.
-
-        # First, we'll check to see if there's already messages our bot knows
-        # of for the team id we've got.
-        if self.messages.get(team_id):
-            # Then we'll update the message dictionary with a key for the
-            # user id we've recieved and a value of a new message object
-            self.messages[team_id].update({user_id: message.Message()})
-        else:
-            # If there aren't any message for that team, we'll add a dictionary
-            # of messages for that team id on our Bot's messages attribute
-            # and we'll add the first message object to the dictionary with
-            # the user's id as a key for easy access later.
-            self.messages[team_id] = {user_id: message.Message()}
-        message_obj = self.messages[team_id][user_id]
-        # Then we'll set that message object's channel attribute to the DM
-        # of the user we'll communicate with
-        message_obj.channel = self.open_dm(user_id)
-        # We'll use the message object's method to create the attachments that
-        # we'll want to add to our Slack message. This method will also save
-        # the attachments on the message object which we're accessing in the
-        # API call below through the message object's `attachments` attribute.
-        message_obj.create_attachments()
-        post_message = self.client.api_call("chat.postMessage",
-                                            channel=message_obj.channel,
-                                            username=self.NAME,
-                                            icon_emoji=self.EMOJI,
-                                            text=message_obj.text,
-                                            attachments=message_obj.attachments
-                                            )
-        timestamp = post_message["ts"]
-        # We'll save the timestamp of the message we've just posted on the
-        # message object which we'll use to update the message after a user
-        # has completed an onboarding task.
-        message_obj.timestamp = timestamp
-
-    def update_share(self, team_id, user_id):
-        """
-        Update onboarding welcome message after recieving a "message" event
-        with an "is_share" attachment from Slack. Update timestamp for
-        welcome message.
-        Parameters
-        ----------
-        team_id : str
-            id of the Slack team associated with the incoming event
-        user_id : str
-            id of the Slack user associated with the incoming event
-        """
-        # These updated attachments use markdown and emoji to mark the
-        # onboarding task as complete
+    def answer(self, team_id, user_id):
         completed_attachments = {"text": ":white_check_mark: "
                                          "~*Share this Message*~ "
                                          ":mailbox_with_mail:",
@@ -166,3 +61,10 @@ class Bot:
                                             )
         # Update the timestamp saved on the message object
         message_obj.timestamp = post_message["ts"]
+
+    def leave_message(self):
+        pass
+
+    def _open_dm(self, user_id: str):
+        new_dm = self.client.api_call("im.open", user=user_id)
+        return new_dm["channel"]["id"]
